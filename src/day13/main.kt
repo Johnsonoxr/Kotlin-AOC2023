de4b6f704b57c5721d97930d1415bdce8cd3a6b4
Johnson1 Chang <Johnson1_Chang@asus.com>
2023-12-13 15:15:36 +0800

    Day13 in progress...

---
+++ src/day13/main.kt
@@ -0,0 +1,75 @@
+package day13
+
+import println
+import readInput
+import kotlin.math.pow
+import kotlin.system.measureNanoTime
+
+private const val FOLDER = "day13"
+
+fun main() {
+    fun part1(input: List<String>): Int {
+
+        fun String.toHash(): Long {
+            if (length > 64) {
+                throw IllegalArgumentException("Row is too long")
+            }
+            return mapIndexed { index, c -> if (c == '#') 2.0.pow(index).toLong() else 0L }.sum()
+        }
+
+        val graphs = mutableListOf<MutableList<String>>(mutableListOf())
+        input.forEach { line ->
+            if (line.isBlank()) {
+                graphs.add(mutableListOf())
+            } else {
+                graphs.last().add(line)
+            }
+        }
+
+        var rowScore = 0
+        var columnScore = 0
+        graphs.forEach { graph ->
+            val rows = graph
+            for (i in 1..<rows.lastIndex) {
+                if (rows.subList(0, i).reversed().zip(rows.subList(i, rows.size)).all { (left, right) -> left == right }) {
+                    rowScore += i
+                    break
+                }
+            }
+
+            val columns = graph[0].indices.map { index -> graph.map { it[index] }.joinToString("") }
+            for (i in 1..<columns.lastIndex) {
+                if (columns.subList(0, i).reversed().zip(columns.subList(i, columns.size)).all { (left, right) -> left == right }) {
+                    columnScore += i
+                    break
+                }
+            }
+        }
+
+        "Row score: $rowScore, column score: $columnScore".println()
+
+        return (rowScore * 100 + columnScore)
+    }
+
+    fun part2(input: List<String>): Int {
+        return 1
+    }
+
+    check(part1(readInput("$FOLDER/test")) == 405)
+    check(part2(readInput("$FOLDER/test")) == 1)
+
+    val input = readInput("$FOLDER/input")
+    val part1Result: Int
+    val part1Time = measureNanoTime {
+        part1Result = part1(input)
+    }
+    val part2Result: Int
+    val part2Time = measureNanoTime {
+        part2Result = part2(input)
+    }
+
+    println("Part 1 result: $part1Result")
+    println("Part 2 result: $part2Result")
+    println("Part 1 takes ${part1Time / 1e6f} milliseconds.")
+    println("Part 2 takes ${part2Time / 1e6f} milliseconds.")
+}
