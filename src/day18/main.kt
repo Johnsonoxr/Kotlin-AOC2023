c833d19f2c7fd304393614d4b9f96eeb9d64b57e
Johnson1 Chang <Johnson1_Chang@asus.com>
2023-12-19 14:51:56 +0800

    Day18 part2 still working...

--- src/day18/main.kt
+++ src/day18/main.kt
@@ -2,75 +2,13 @@ package day18
 
 import println
 import readInput
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.system.exitProcess
 import kotlin.system.measureNanoTime
 
 private const val FOLDER = "day18"
-
-class TwoDimenGraph<T>(graph: Collection<T>, val stride: Int) {
-
-    inner class Position(val y: Int, val x: Int) {
-
-        val info: MutableMap<String, Any> by lazy { mutableMapOf() }
-
-        fun up(offset: Int = 1) = if (y - offset >= 0) Position(y - offset, x) else null
-        fun down(offset: Int = 1) = if (y + offset < h) Position(y + offset, x) else null
-        fun left(offset: Int = 1) = if (x - offset >= 0) Position(y, x - offset) else null
-        fun right(offset: Int = 1) = if (x + offset < w) Position(y, x + offset) else null
-
-        fun neighbors() = listOfNotNull(up(), down(), left(), right())
-
-        fun createIntervalPositionsTo(other: Position): List<Position> {
-            val positions = mutableListOf<Position>()
-            var currentP = this
-            while (currentP != other) {
-                currentP = when {
-                    other.y > currentP.y -> currentP.down()!!
-                    other.y < currentP.y -> currentP.up()!!
-                    other.x > currentP.x -> currentP.right()!!
-                    other.x < currentP.x -> currentP.left()!!
-                    else -> throw Exception("Not gonna happen")
-                }
-                positions.add(currentP)
-            }
-            return positions
-        }
-
-        override fun toString() = "P($y, $x)"
-
-        override fun equals(other: Any?): Boolean {
-            if (other !is TwoDimenGraph<*>.Position) return false
-            return y == other.y && x == other.x
-        }
-
-        override fun hashCode(): Int {
-            return stride * y + x
-        }
-    }
-
-    fun createPosition(y: Int, x: Int, dir: String = "") = Position(y, x)
-
-    val graph = graph.toMutableList()
-    val w = stride
-    val h = graph.size / stride
-
-    operator fun get(y: Int, x: Int): T {
-        return graph[y * stride + x]
-    }
-
-    operator fun get(position: Position): T {
-        return get(position.y, position.x)
-    }
-
-    operator fun set(position: Position, value: T): Boolean {
-        graph[position.y * stride + position.x] = value
-        return true
-    }
-
-    override fun toString(): String {
-        return graph.chunked(stride).joinToString("\n") { it.joinToString("") }
-    }
-}
 
 data class Move(val dir: String, val steps: Long)
 
@@ -82,6 +20,21 @@ data class Position(val y: Long, val x: Long) {
         "R" -> Position(y, x + move.steps)
         else -> throw Exception("Not gonna happen")
     }
+
+    fun isInside(rectangle: Rectangle): Boolean {
+        return x in rectangle.left + 1..<rectangle.right && y in rectangle.top + 1..<rectangle.bottom
+    }
+
+    operator fun plus(other: Position) = Position(y + other.y, x + other.x)
+    operator fun minus(other: Position) = Position(y - other.y, x - other.x)
+}
+
+data class Edge(val from: Position, val to: Position) {
+    val length = abs(from.x - to.x) + abs(from.y - to.y)
+}
+
+data class Rectangle(val left: Long, val top: Long, val right: Long, val bottom: Long) {
+    val area = (right - left) * (bottom - top)
 }
 
 val ccwTurns = mapOf(
@@ -94,8 +47,6 @@ val ccwTurns = mapOf(
 fun main() {
 
     fun toCcwMoves(moves: List<Move>): List<Move> {
-
-
         val isCcw = moves.windowed(2).sumOf { (m1, m2) ->
             when (m2.dir) {
                 ccwTurns[m1.dir] -> 1
@@ -118,82 +69,151 @@ fun main() {
 
     fun solve(moves: List<Move>): Long {
 
-        data class Movement(val x: Long, val y: Long, val dir: String, val steps: Long) {
-            fun rangeX(): LongRange = when (dir) {
-                "L" -> x - steps..x
-                "R" -> x..x + steps
-                else -> throw Exception("Not gonna happen")
-            }
-
-            fun rangeY(): LongRange = when (dir) {
-                "U" -> y - steps..y
-                "D" -> y..y + steps
-                else -> throw Exception("Not gonna happen")
-            }
-        }
-
-        fun LongRange.isIntersect(other: LongRange): Boolean {
-            return this.first <= other.last && this.last >= other.first
-        }
+        val edges = mutableListOf<Edge>()
 
         val ccwMoves = toCcwMoves(moves)
 
-        val newMoves = mutableListOf<Move>()
-
+        var startP = Position(0, 0)
         (ccwMoves.takeLast(1) + ccwMoves + ccwMoves.take(1)).windowed(3).forEach { (m1, m2, m3) ->
-
             val isTurn1Ccw = ccwTurns[m1.dir] == m2.dir
             val isTurn2Ccw = ccwTurns[m2.dir] == m3.dir
-            val stepFix = when {
+            val edgeLength = when {
                 isTurn1Ccw && isTurn2Ccw -> m2.steps + 1
                 isTurn1Ccw || isTurn2Ccw -> m2.steps
                 else -> m2.steps - 1
             }
-
-            newMoves.add(Move(m2.dir, stepFix))
+            val endP = startP.move(Move(m2.dir, edgeLength))
+            edges.add(Edge(startP, endP))
+            startP = endP
         }
 
-        val movePosition = mutableListOf(Position(0, 0))
-        newMoves.dropLast(1).forEach { move ->
-            movePosition.add(movePosition.last().move(move))
-        }
+        val rectangles = mutableListOf<Rectangle>()
 
-        val movements = newMoves.zip(movePosition).map { (move, position) ->
-            Movement(position.x, position.y, move.dir, move.steps)
-        }
-
-        movements.forEach { it.println() }
-        exitProcess(0)
-
-        data class Rectangle(val left: Long, val top: Long, val right: Long, val bottom: Long) {
-            val area = (right - left + 1) * (bottom - top + 1)
-        }
-
-        val rectangles = mutableSetOf<Rectangle>()
-
-        var closedMovementsList = mutableListOf(movements)
-
-        while (closedMovementsList.isNotEmpty()) {
-            val newClosedList = closedMovementsList.drop(1).toMutableList()
-
-            val firstMovements = closedMovementsList.first()
-            val theMovement = firstMovements.filter { it.dir == "U" }.maxBy { it.y }
-
-            val rectLeft = firstMovements.filter { m ->
-                m.dir == "D" && m.x < theMovement.x && m.rangeY().isIntersect(theMovement.rangeY())
-            }.maxOf { it.x }
-
-            val rect = Rectangle(rectLeft, theMovement.y - theMovement.steps, theMovement.x, theMovement.y)
-            rectangles.add(rect)
-
-            var newClosedMovements: MutableList<Movement> = mutableListOf()
-            firstMovements.forEach { m ->
-                if (m.dir == "D" && m.rangeY().isIntersect(theMovement.rangeY())) {
-                    newClosedMovements.dropLast(1)
-                }
+        fun Edge.describe(): String {
+            return when {
+                from.x < to.x -> "R"
+                from.x > to.x -> "L"
+                from.y < to.y -> "D"
+                from.y > to.y -> "U"
+                else -> throw Exception("Not gonna happen")
             }
+        }
 
-            closedMovementsList = newClosedList
+        fun Edge.cross(edge: Edge): Boolean {
+            return when {
+                from.x == to.x && edge.from.x == edge.to.x -> false
+                from.y == to.y && edge.from.y == edge.to.y -> false
+                from.x == to.x -> {
+                    val x = from.x
+                    val y = edge.from.y
+                    val y1 = min(from.y, to.y)
+                    val y2 = max(from.y, to.y)
+                    val x1 = min(edge.from.x, edge.to.x)
+                    val x2 = max(edge.from.x, edge.to.x)
+                    x in x1 + 1..<x2 && y in y1 + 1..<y2
+                }
+                from.y == to.y -> {
+                    val y = from.y
+                    val x = edge.from.x
+                    val x1 = min(from.x, to.x)
+                    val x2 = max(from.x, to.x)
+                    val y1 = min(edge.from.y, edge.to.y)
+                    val y2 = max(edge.from.y, edge.to.y)
+                    x in x1 + 1..<x2 && y in y1 + 1..<y2
+                }
+                else -> throw Exception("Not gonna happen")
+            }
+        }
+
+        while (edges.isNotEmpty()) {
+
+            "Edges: ${edges.size}".println()
+
+            var changed = false
+            for ((e0, e1, e2, e3, e4) in (edges.takeLast(2) + edges + edges.take(2)).windowed(5)) {
+
+                val offset1 = e1.to - e1.from
+                val offset2 = e2.to - e2.from
+                val offset3 = e3.to - e3.from
+
+                // Check if the edges goes in counter-clockwise direction
+                if (offset1.y < 0 && offset2.x > 0  // U -> R
+                    || offset1.x < 0 && offset2.y < 0   // L -> U
+                    || offset1.y > 0 && offset2.x < 0   // D -> L
+                    || offset1.x > 0 && offset2.y > 0   // R -> D
+                ) {
+                    continue
+                }
+
+                // Check if the edges goes in counter-clockwise direction
+                if (offset2.y < 0 && offset3.x > 0  // U -> R
+                    || offset2.x < 0 && offset3.y < 0   // L -> U
+                    || offset2.y > 0 && offset3.x < 0   // D -> L
+                    || offset2.x > 0 && offset3.y > 0   // R -> D
+                ) {
+                    continue
+                }
+
+                val rectangleCornerPair = when {
+                    e1.length < e3.length -> Pair(e1.from, e2.to)
+                    else -> Pair(e2.from, e3.to)
+                }
+
+                val rectangle = Rectangle(
+                    min(rectangleCornerPair.first.x, rectangleCornerPair.second.x),
+                    min(rectangleCornerPair.first.y, rectangleCornerPair.second.y),
+                    max(rectangleCornerPair.first.x, rectangleCornerPair.second.x),
+                    max(rectangleCornerPair.first.y, rectangleCornerPair.second.y)
+                )
+
+                if (edges.any { it.from.isInside(rectangle) }) {
+                    continue
+                }
+
+                rectangles.add(rectangle)
+
+                val insertIdx = edges.indexOf(e2)
+                when {
+                    e1.length < e3.length -> {
+                        val newEdge = Edge(e0.from, e2.to + e1.from - e1.to)
+                        val newEdge2 = Edge(newEdge.to, e4.from)
+                        edges.add(insertIdx, newEdge2)
+                        edges.add(insertIdx, newEdge)
+                        edges.remove(e2)
+                        edges.remove(e0)
+                        edges.remove(e1)
+                        edges.remove(e3)
+                    }
+
+                    e1.length > e3.length -> {
+                        val newEdge = Edge(e2.from + e3.to - e3.from, e4.to)
+                        val newEdge2 = Edge(e0.to, newEdge.from)
+                        edges.add(insertIdx, newEdge)
+                        edges.add(insertIdx, newEdge2)
+                        edges.remove(e2)
+                        edges.remove(e1)
+                        edges.remove(e3)
+                        edges.remove(e4)
+                    }
+
+                    else -> {
+                        val newEdge = Edge(e0.from, e4.to)
+                        edges.add(insertIdx, newEdge)
+                        edges.remove(e2)
+                        edges.remove(e0)
+                        edges.remove(e1)
+                        edges.remove(e3)
+                        edges.remove(e4)
+                    }
+                }
+
+                changed = true
+
+                break
+            }
+            if (!changed) {
+                throw Exception("Wryyyyyyyy")
+            }
         }
 
         return rectangles.sumOf { it.area }
